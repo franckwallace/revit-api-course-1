8acfcc122c12df964e0c369f4bed40eb2d54112b
Franck Wallace <franckwallace.fw@outlook.com>
2025-04-30 00:11:30 -0300

    Initial commit

---
+++ CoreRevitModule/CoreRevitModule/Main.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.Creation;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+
+
+namespace CoreRevitModule
+
+#region about transactions mode
+
+//if transaction is set to read-only - the command cannot have any transactions
+//and cannot call any methods that modify a document
+//while this external command is being executed, nobody elsy is allowed to modify the document
+//or have a transaction either
+//To out it simply, this guarantees that during the entire execution of the external command
+//the current models all remain unmodified
+
+#endregion
+
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            var uiapp = commandData.Application;
+            var app = uiapp.Application;
+            var uidoc = uiapp.ActiveUIDocument;
+            var doc = uidoc.Document;
+
+            var selectedElement = uidoc.Selection.GetElementIds().Select(
+                x => doc.GetElement(x)).First();
+
+            var value = selectedElement.LookupParameter("Comentários").AsString();
+            TaskDialog.Show("Message", value);
+
+
+
+            //var families = new List<Element>();
+            //foreach (var familyId in familyIds)
+            //{
+            //    families.Add(
+            //        doc.GetElement(familyId));
+            //}
+
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ CoreRevitModule/CoreRevitModule/UserWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections;
+
+namespace CoreRevitModule
+{
+    /// <summary>
+    /// Interaction logic for UserWindow.xaml
+    /// </summary>
+    public partial class UserWindow : Window
+    {
+        public UserWindow(IEnumerable collection)
+        {
+            InitializeComponent();
+            Box.ItemsSource = collection;
+            //Box.DisplayMemberPath = "Name";
+
+        }
+    }
+}
